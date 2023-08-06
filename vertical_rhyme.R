2783f8d8b42f7602912b6420099640cd7ec8bdc2
Roman Matvienko <10424604+rmatv@users.noreply.github.com>
2023-08-06 12:48:09 +0800

    Delete vertical_rhyme.R

--- vertical_rhyme.R
+++
@@ -1,8 +0,0 @@
-VerticalRhythm <- function(base.size, base.line, font.size) {
-  base.number = base.line / base.size
-  line.height = base.size * base.number / font.size
-  font.size = font.size / base.size
-  return(c(font.size, line.height))
-}
-
-VerticalRhythm(16, 24, 14)
